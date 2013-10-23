12ca3d5a15074ce35c991ebcb762baa48d010b75
Joe Ellis <joe@joeellis.la>
2013-10-22 21:28:39 -0500

    fix datE

--- app.rb
+++ app.rb
@@ -8,10 +8,11 @@ class FrontEndParty < Sinatra::Base
   end
 
   get "/" do
-    current_month = Time.now.strftime "%B"
-    unless next_meeting = Chronic.parse("fifth Thursday in #{current_month}")
-      next_meeting = Chronic.parse "fourth Thursday in #{current_month}"
-    end
+    next_meeting = Chronic.parse "October 24th, 2013"
+    # current_month = Time.now.strftime "%B"
+    # unless next_meeting = Chronic.parse("fifth Thursday in #{current_month}")
+    #   next_meeting = Chronic.parse "fourth Thursday in #{current_month}"
+    # end
     erb :index, :locals => {:next_meeting => next_meeting.strftime("%B %e, %Y") }
   end
 end
