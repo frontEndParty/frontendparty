049bb0b9f16adfef1fe31010769e65c484c7154e
Joe Ellis <joe@joeellis.la>
2013-10-22 21:30:43 -0500

    fix date

--- app.rb
+++ app.rb
@@ -7,12 +7,18 @@ class FrontEndParty < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  def next_meeting custom_date
+    if custom_date
+      next_meeting = Chronic.parse custom_date
+    else
+      current_month = Time.now.strftime "%B"
+      unless next_meeting = Chronic.parse("fifth Thursday in #{current_month}")
+        next_meeting = Chronic.parse "fourth Thursday in #{current_month}"
+      end
+    end
+  end
+
   get "/" do
-    next_meeting = Chronic.parse "October 24th, 2013"
-    # current_month = Time.now.strftime "%B"
-    # unless next_meeting = Chronic.parse("fifth Thursday in #{current_month}")
-    #   next_meeting = Chronic.parse "fourth Thursday in #{current_month}"
-    # end
-    erb :index, :locals => {:next_meeting => next_meeting.strftime("%B %e, %Y") }
+    erb :index, :locals => {:next_meeting => next_meeting("October 24th 2013").strftime("%B %e, %Y") }
   end
 end
