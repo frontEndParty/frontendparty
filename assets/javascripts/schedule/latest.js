b100db45ddb2d2ee509c6d1890f4fe72d6f68d08
Matt Shwery <mshwery@gmail.com>
2016-08-21 15:10:21 -0500

    move schedule of speakers to retain archive, adjust css, add talks for august (#31)

---
+++ assets/javascripts/schedule/latest.js
@@ -0,0 +1,62 @@
+/**
+ * @overview  latest.js is where we can keep the upcoming speaker json data
+ *            when creating a new set of speakers for the next meetup
+ *            move the contents of this file to a json file with that 
+ *            meetup's date as the file name, e.g. `2016-07-28.json`
+ */
+var people = [
+  {
+    "name": "Sia Karamalegos",
+    "portrait": "https://media.licdn.com/media/p/2/000/063/3b5/12bd7fb.jpg",
+    "presentation": {
+      "title": "Getting started with React",
+      "description": "Have you been interested in learning about React? Don't know where to start? I'll share the ups and downs of learning React on your own and share some great resources and what order to try to learn things in."
+    },
+    "company": {
+      "url": "http://revelry.co/",
+      "name": "Revelry",
+    },
+    "social": {
+      "twitter": "https://twitter.com/thegreengreek",
+      "facebook": "",
+      "globe": "",
+      "github": "https://github.com/Siakaramalegos",
+    }
+  },
+  {
+    "name": "Steve Price",
+    "portrait": "https://avatars2.githubusercontent.com/u/1231168?v=3&s=460",
+    "presentation": {
+      "title": "Co.js for fun and profit",
+      "description": "Callback Hell got you down? Get a taste of the buttery smooth future of ES7 async programming today with co.js."
+    },
+    "company": {
+      "url": "http://www.nola.gov",
+      "name": "City of New Orleans",
+    },
+    "social": {
+      "twitter": "https://twitter.com/themooserooster",
+      "facebook": "",
+      "globe": "",
+      "github": "https://github.com/themooserooster",
+    }
+  },
+  {
+    "name": "Colin Lacy",
+    "portrait": "https://avatars3.githubusercontent.com/u/4993605?v=3&s=460",
+    "presentation": {
+      "title": "TypeScript: Why I Drank the Kool Aid",
+      "description": "This is my story of how and why I got hooked on TypeScript, and why my team is transitioning all of our front-end and Node development to TS. Told from my own experience, with absolutely no Googling of evangelistic blog posts. Just my explanation of why it works for me, and why it might work for you."
+    },
+    "company": {
+      "url": "https://www.gepower.com/",
+      "name": "GE Power",
+    },
+    "social": {
+      "twitter": "",
+      "facebook": "",
+      "globe": "",
+      "github": "https://github.com/colinjlacy",
+    }
+  }
+];
