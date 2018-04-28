b47227d58ba3e41a499c97ac37c2be85ad13b79a
Cameron Sampson <csampson.js@gmail.com>
2018-04-27 19:26:32 -0500

    Cleared scheduled talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -4,40 +4,5 @@
  *            move the contents of this file to a json file with that
  *            meetup's date as the file name, e.g. `2016-07-28.json`
  */
-var people = [
-  {
-    "name": "Colin Lacy",
-    "portrait": "https://avatars2.githubusercontent.com/u/4993605?s=460&v=4",
-    "presentation": {
-      "title": "Implementing Material Design - Why, When, and How",
-      "description": "A very light introduction to Material Design; an overview of some tools that can help kickstart development and example implementations using frameworks you already know."
-    },
-    "social": {
-      "github": "https://github.com/colinjlacy"
-    }
-  },
-  {
-    "name": "George Mauer",
-    "portrait": "https://avatars3.githubusercontent.com/u/66150?s=460&v=4",
-    "presentation": {
-      "title": "There Is No Front-End",
-      "description": "Does it make sense to distinguish so heavily between backend/frontend dev these days? Modern tooling and universal JavaScript gives us reason for doubt. George will give us his take on it all."
-    },
-    "social": {
-      "github": "https://github.com/togakangaroo",
-      "twitter": "https://twitter.com/togakangaroo"
-    }
-  },
-  {
-    "name": "Cameron Sampson",
-    "portrait": "https://avatars0.githubusercontent.com/u/2473115?v=3&s=460",
-    "presentation": {
-      "title": " WebSockets: Building Realtime Apps",
-      "description": "An introduction to WebSockets, plus we'll take a look at how to build a simple realtime app (okay fine, it's actually gonna be a multiplayer browser game)."
-    },
-    "social": {
-      "globe": "http://cameronsampson.com",
-      "github": "https://github.com/csampson"
-    }
-  }
-];
+var people = [];
+
