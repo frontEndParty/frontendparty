c85e71f0d917d43ceb2f394a943f4d86a9b9f26e
Cameron Sampson <csampson.js@gmail.com>
2018-07-22 19:18:59 -0500

    July 2018 talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -4,5 +4,27 @@
  *            move the contents of this file to a json file with that
  *            meetup's date as the file name, e.g. `2016-07-28.json`
  */
-var people = [];
-
+var people = [
+  {
+    "name": "George Mauer",
+    "portrait": "https://avatars3.githubusercontent.com/u/66150?s=460&v=4",
+    "presentation": {
+      "title": "Storybook, An Interactive UI Component Style Guide",
+      "description": "George will introduce us to Storybook (React), a super useful tool that lets you view different UI component states for development and style guide purposes."
+    },
+    "social": {
+      "github": "https://github.com/togakangaroo"
+    }
+  },
+  {
+    "name": "Colby Taperts",
+    "portrait": "https://avatars2.githubusercontent.com/u/6157465?s=460&v=4",
+    "presentation": {
+      "title": "React Transition Groups",
+      "description": "Take control of your React animations with transition groups, a simple but powerful way to control animations based on state and component changes."
+    },
+    "social": {
+      "github": "https://github.com/ctaperts"
+    }
+  }
+];
