a311918d2aba94e5a8dd1ec32098cdd51bce6346
Cameron Sampson <csampson.js@gmail.com>
2018-10-22 22:01:59 -0500

    Updated October 2018 talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -6,6 +6,19 @@
  */
 var people = [
   {
+    "name": "Sia Karamalegos",
+    "portrait": "https://avatars3.githubusercontent.com/u/4777393?v=3&s=460",
+    "presentation": {
+      "title": "Real User Monitoring",
+      "description": "What is Real User Monitoring versus Synthetic Testing in the realm of front-end performance? What are some of the tools available to measure both? In this talk we'll do an overview of measurement methods and do a deep-dive on the User Timing API."
+    },
+    "social": {
+      "globe": "http://siakaramalegos.github.io/",
+      "github": "https://github.com/siakaramalegos",
+      "twitter": "https://twitter.com/thegreengreek"
+    }
+  },
+  {
     "name": "Edwin Gomez",
     "portrait": "https://media.licdn.com/dms/image/C5603AQGmdty2foXsCg/profile-displayphoto-shrink_800_800/0?e=1545868800&v=beta&t=IJQByKKQjdXrOrftDMsx-f-FrEEzTNZHxO8rliOqppM",
     "presentation": {
@@ -24,6 +37,7 @@ var people = [
       "description": "We'll be taking a look at how games such as Borderlands 2 and Skyrim tackle both generic and their own unique UI and UX challenges, with a live playthrough and commentary."
     },
     "social": {
+      "globe": "http://cameronsampson.com",
       "github": "https://github.com/csampson"
     }
   }
