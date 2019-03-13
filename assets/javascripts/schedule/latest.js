fc2c487632184af52f763cb59b3c05c09a8f6c53
Cameron Sampson <csampson.js@gmail.com>
2019-03-13 09:28:37 -0500

    Cleared old talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -5,40 +5,4 @@
  *            meetup's date as the file name, e.g. `2016-07-28.json`
  */
 var people = [
-  {
-    "name": "Sia Karamalegos",
-    "portrait": "https://avatars3.githubusercontent.com/u/4777393?v=3&s=460",
-    "presentation": {
-      "title": "Real User Monitoring",
-      "description": "What is Real User Monitoring versus Synthetic Testing in the realm of front-end performance? What are some of the tools available to measure both? In this talk we'll do an overview of measurement methods and do a deep-dive on the User Timing API."
-    },
-    "social": {
-      "globe": "http://siakaramalegos.github.io/",
-      "github": "https://github.com/siakaramalegos",
-      "twitter": "https://twitter.com/thegreengreek"
-    }
-  },
-  {
-    "name": "Edwin Gomez",
-    "portrait": "https://media.licdn.com/dms/image/C5603AQGmdty2foXsCg/profile-displayphoto-shrink_800_800/0?e=1545868800&v=beta&t=IJQByKKQjdXrOrftDMsx-f-FrEEzTNZHxO8rliOqppM",
-    "presentation": {
-      "title": "Email Rendering & Cross-Client Testing",
-      "description": "Edwin will cover some of the email rendering tech they use at rasa.io: the various pain points with cross-client support and how to test email rendering across different clients."
-    },
-    "social": {
-      "linkedin": "https://linkedin.com/in/edwin-gomez-a67519101"
-    }
-  },
-  {
-    "name": "Cameron Sampson",
-    "portrait": "https://avatars1.githubusercontent.com/u/2473115?s=460&v=4",
-    "presentation": {
-      "title": "Analyzing Video Game UI/UX Patterns",
-      "description": "We'll be taking a look at how games such as Borderlands 2 and Skyrim tackle both generic and their own unique UI and UX challenges, with a live playthrough and commentary."
-    },
-    "social": {
-      "globe": "http://cameronsampson.com",
-      "github": "https://github.com/csampson"
-    }
-  }
 ];
