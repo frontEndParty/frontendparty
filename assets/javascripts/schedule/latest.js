7d0a697f3d0a81cbc3ef367462da2813bfbbef85
Cameron Sampson <csampson.js@gmail.com>
2019-03-22 11:34:24 -0500

    March 2019 talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -5,4 +5,28 @@
  *            meetup's date as the file name, e.g. `2016-07-28.json`
  */
 var people = [
+  {
+    "name": "Chris Guzman",
+    "portrait": "https://ca.slack-edge.com/T028DDR96-U4L3EC4K1-25d6e42228c9-512",
+    "presentation": {
+      "title": "Kotlin for JavaScript",
+      "description": "Chris will introduce us to Kotlin, a cross-platform, statically-typed programming language that targets the JVM, Android, Native, and JavaScript."
+    },
+    "social": {
+      "globe": "http://chris-guzman.com",
+      "twitter": "https://twitter.com/speaktochris"
+    }
+  },
+  {
+    "name": "Cameron Sampson",
+    "portrait": "https://avatars1.githubusercontent.com/u/2473115?s=460&v=4",
+    "presentation": {
+      "title": "Game UX In-Depth: Overwatch",
+      "description": "We'll be taking an in-depth look at how the game Overwatch (PC) uses audio and other cues to feed players information in a fast-paced and distraction-heavy environment."
+    },
+    "social": {
+      "globe": "http://cameronsampson.com",
+      "github": "https://github.com/csampson"
+    }
+  }
 ];
