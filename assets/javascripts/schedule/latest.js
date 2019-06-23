9bd78ca8798e1027b195cf7232f8648bf80d60b4
Cameron Sampson <csampson.js@gmail.com>
2019-06-22 22:54:01 -0500

    June 2019 talks

--- assets/javascripts/schedule/latest.js
+++ assets/javascripts/schedule/latest.js
@@ -5,4 +5,43 @@
  *            meetup's date as the file name, e.g. `2016-07-28.json`
  */
 var people = [
+  {
+    "name": "George Mauer",
+    "portrait": "https://avatars3.githubusercontent.com/u/66150?s=460&v=4",
+    "presentation": {
+      "title": "There Is No Front-End",
+      "description": "Does it make sense to distinguish so heavily between backend/frontend dev these days? Modern tooling and universal JavaScript give us reason for doubt. George will give us his take on it all."
+    },
+    "social": {
+      "globe": "http://georgemauer.net/",
+      "github": "https://github.com/togakangaroo",
+      "linkedin": "https://www.linkedin.com/in/george-mauer/"
+    }
+  },
+  {
+    "name": "Olivia Sabo-Rush",
+    "portrait": "https://avatars0.githubusercontent.com/u/23201987?s=460&v=4",
+    "presentation": {
+      "title": "Understanding and Using Animation in CSS/JS",
+      "description": "Animation has the potential to create a meaningful impact in any website or application’s user experience. From elaborate designs, to a simple loading spinner, animation can help demonstrate important signals without the use of text. We’ll explore what animation is, the different ways we can incorporate it in CSS or JavaScript, and several libraries to try out."
+    },
+    "social": {
+      "globe": "http://pafiu.me",
+      "github": "https://github.com/livrush",
+      "linkedin": "https://www.linkedin.com/in/livrush"
+    }
+  },
+  {
+    "name": "Cameron Sampson",
+    "portrait": "https://avatars1.githubusercontent.com/u/2473115?s=460&v=4",
+    "presentation": {
+      "title": "An Introduction To Web Accessibility",
+      "description": "Web accessibility is the practice of ensuring web content and functionality is able to by accessed by all users, regardless of disability. This talk will introduce developers and designers to the concept, walk through a number of best practices around semantics, structure, ARIA, and a live demo of a screen-reader."
+    },
+    "social": {
+      "globe": "http://cameronsampson.com",
+      "github": "https://github.com/csampson",
+      "linkedin": "https://www.linkedin.com/in/cameron-adrian-sampson",
+    }
+  }
 ];
